3b0ef6538eda29001f815591006303d4e9670638
mahfouz72 <mohamadmahfouz39@gmail.com>
2023-03-20 19:21:30 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Discription: chech for number
+  *Return: always 0
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 58; n++)
+	{
+		putchar(n);
+		if(n <= 56 )
+			putchar(',');
+			putchar(32);
+	}
+	putchar('\n');
+	return (0);
+}
