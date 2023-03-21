79cb9784059582edf6c9f480d9a5a1e0ae37c7af
mahfouz72 <mohamadmahfouz39@gmail.com>
2023-03-20 18:57:33 -0700

    4-print_numbers.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Discription: chech for number
+  *Return: always 0
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
