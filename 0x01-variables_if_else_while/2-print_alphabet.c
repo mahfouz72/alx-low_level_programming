6e67a57ff97d260bfccf4f8de7c971efe9ca29f8
mahfouz72 <mohamadmahfouz39@gmail.com>
2023-03-20 18:35:43 -0700

    alphapet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Discription: chech for number
+  *Return: always 0
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
