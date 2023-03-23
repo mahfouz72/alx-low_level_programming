4f47def20090e167a90abbb9ddc0e803b2ea8fe2
mahfouz72 <mohamadmahfouz39@gmail.com>
2023-03-22 18:33:02 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  * print_alphabet_x10 - prints alphabet 10 times
+  *
+  * Return: Always 0 (success)
+  */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (j = 1; j <= 10; j++)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
