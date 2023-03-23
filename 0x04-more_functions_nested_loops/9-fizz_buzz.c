0ae48ea6fa262b4fdab5b5b69a6b2888d01f5129
mahfouz72 <mohamadmahfouz39@gmail.com>
2023-03-23 06:23:26 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -19,8 +19,8 @@ int main(void)
 		else if (i % 3 == 0 && i % 5 == 0)
 			printf(" FizzBuzz");
 		else
-			printf("%d", i);
+			printf("%d ", i);
 	}
-	printf('\n');
+	printf("\n");
 	return (0);
 }
